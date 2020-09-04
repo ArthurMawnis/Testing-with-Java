b1b076b614b408174e9120a35ef3f608a7c2d000
Arthur <arthur.cdfreitas@gmail.com>
2020-09-03 21:55:20 -0300

    fix: add validation for inequality

--- src/main/java/com/arthur/utils/TriangleUtils.java
+++ src/main/java/com/arthur/utils/TriangleUtils.java
@@ -12,18 +12,18 @@ public class TriangleUtils {
 	private static final String ISOSCELES = "isosceles";
 	private static final String EQUILATERAL = "equilateral";
 	private static final String SCALENE = "scalene";
-	
-	private TriangleUtils() {}
+
+	private TriangleUtils() {
+	}
 
 	/**
-	 * Verify if a triangle is:
-	 * 1) A Scalene when all sides are different
-	 * 2) A Equilateral when all sides are equals
-	 * 3) A Isosceles when theres two equal sides
+	 * Verify if a triangle is: 1) A Scalene when all sides are different 2) A
+	 * Equilateral when all sides are equals 3) A Isosceles when theres two equal
+	 * sides
 	 * 
-	 * @param First side
+	 * @param First  side
 	 * @param Second side
-	 * @param Third side
+	 * @param Third  side
 	 * @return Triangle type
 	 * @throws NoPossibleTriangleException
 	 */
@@ -34,18 +34,32 @@ public class TriangleUtils {
 			return EQUILATERAL;
 		}
 
-		if ((s1.equals(s2) && !s1.equals(s3)) 
-				|| (s1.equals(s3) && !s1.equals(s2))
+		if ((s1.equals(s2) && !s1.equals(s3)) || (s1.equals(s3) && !s1.equals(s2))
 				|| (s2.equals(s3) && !s1.equals(s2))) {
+
+			if (!assertInequality(s1, s2, s3))
+				throw new NoPossibleTriangleException();
+
 			System.out.println(ISOSCELES);
 			return ISOSCELES;
 		}
 
 		if (!s1.equals(s2) && !s1.equals(s3) && !s2.equals(s3)) {
+			if (!assertInequality(s1, s2, s3))
+				throw new NoPossibleTriangleException();
+
 			System.out.println(SCALENE);
 			return SCALENE;
 		}
 
 		throw new NoPossibleTriangleException();
+	}
+
+	private static boolean assertInequality(final Float s1, final Float s2, final Float s3) {
+		boolean a = s1 < s2 + s3;
+		boolean b = s2 < s1 + s3;
+		boolean c = s3 < s1 + s2;
+
+		return a && b && c;
 	}
 }
