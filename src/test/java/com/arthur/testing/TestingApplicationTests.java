7c0081e1558b3244d9cfe4ddc8da2b9ca337d63c
Arthur <arthur.cdfreitas@gmail.com>
2020-09-04 22:19:53 -0300

    add test case for a + b < c within permutation

--- src/test/java/com/arthur/testing/TestingApplicationTests.java
+++ src/test/java/com/arthur/testing/TestingApplicationTests.java
@@ -166,6 +166,16 @@ class TestingApplicationTests {
 
 	@Test
 	void should_throw_exception_when_sum_of_two_is_greater_than_third_side_within_permutation() {
+		final Float a = 1f;
+		final Float b = 2f;
+		final Float c = 4f;
+
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(a, b, c));
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(a, c, b));
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(b, a, c));
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(b, c, a));
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(c, a, b));
+		Assertions.assertThrows(NoPossibleTriangleException.class, () -> TriangleUtils.printTriangleType(c, b, a));
 	}
 
 	@Test
